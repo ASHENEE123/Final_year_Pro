c8efbb0318cf05e28732908c667721855d9c3012
ASHENEE123 <159702514+ASHENEE123@users.noreply.github.com>
2025-03-18 23:11:37 +0530

    Update App.py

--- App.py
+++ App.py
@@ -14,7 +14,8 @@ app = FastAPI()
 # Enable CORS for React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://brainstrokedetection.onrender.com"],  # Replace with your React frontend URL e.g., ["http://localhost:3000"]
+    allow_origins=["https://brainstrokedetection.onrender.com",
+                  "http://localhost:3000/"],  # Replace with your React frontend URL e.g., ["http://localhost:3000"]
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
