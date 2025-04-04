59d6f401486950689ec04a573c236eb5e032d3ba
ASHENEE123 <159702514+ASHENEE123@users.noreply.github.com>
2025-04-04 23:11:22 +0530

    Update App.py

--- App.py
+++ App.py
@@ -22,6 +22,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+     expose_headers=["X-Predicted-Class", "X-Confidence-Score"],
 )
 
 def preprocess_image(image_data):
